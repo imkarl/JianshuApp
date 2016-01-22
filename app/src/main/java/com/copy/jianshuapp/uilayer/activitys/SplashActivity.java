78f29dd06cf6a953f34b9a78126e5c0ada029d8f
alafighting <alafighting@163.com>
2016-01-22 13:48:14 +0800

    完成启动页；增加RxAndroid依赖

--- app/src/main/java/com/copy/jianshuapp/uilayer/activitys/SplashActivity.java
+++ app/src/main/java/com/copy/jianshuapp/uilayer/activitys/SplashActivity.java
@@ -5,13 +5,26 @@ import android.os.Bundle;
 import com.copy.jianshuapp.uilayer.BaseActivity;
 import com.copy.jianshuapp.uilayer.UIHelper;
 
+import java.util.concurrent.TimeUnit;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+
 public class SplashActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        UIHelper.startMain(getActivity());
-        finish();
+
+        rx.Observable.timer(1000, TimeUnit.MILLISECONDS)
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Long>() {
+            @Override
+            public void call(Long o) {
+                UIHelper.startMain(getActivity());
+                finish();
+            }
+        });
     }
 
 }
