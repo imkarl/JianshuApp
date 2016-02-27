44c1d5e87be9e1b3211a4bdcba9baf5f504a0bdd
alafighting <alafighting@163.com>
2016-02-27 18:10:17 +0800

    补充提交

--- app/src/main/java/com/copy/jianshuapp/uilayer/BaseActivity.java
+++ app/src/main/java/com/copy/jianshuapp/uilayer/BaseActivity.java
@@ -1,8 +1,13 @@
 package com.copy.jianshuapp.uilayer;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.copy.jianshuapp.utils.version.AndroidVersion;
 
 /**
  * Activity基类
@@ -17,5 +22,17 @@ public class BaseActivity extends AppCompatActivity {
     public Activity getActivity() {
         return this;
     }
+
+    public <T extends android.support.v4.app.Fragment> T findSupportFragment(Class<T> fragmentClass) {
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        android.support.v4.app.Fragment fragment = fragmentManager.findFragmentByTag(fragmentClass.getName());
+        return (T) fragment;
+    }
+
+    public <T extends android.support.v4.app.Fragment> T newSupportFragment(Class<T> fragmentClass, Bundle args) {
+        android.support.v4.app.Fragment fragment = android.support.v4.app.Fragment.instantiate(getContext(), fragmentClass.getName(), args);
+        return (T) fragment;
+    }
+
 
 }
